318cca595065db5d5b21fbf550a5aba110bbece2
ljh8450 <dzigh30@gmail.com>
2023-12-16 00:11:51 +0900

    Update 2178.py

--- 2178.py
+++ 2178.py
@@ -1,7 +1,27 @@
 from sys import stdin
+from collections import deque
 
 def split_int(input_int): #split int function
     return [int(i) for i in input_int]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 1:
+                queue.append((nx, ny))
+                data[nx][ny] = data[x][y] + 1 
+    return data[n-1][m-1]
 
 
 n, m = map(int, stdin.readline().split())
@@ -9,4 +29,4 @@ data = list()
 for i in range(n):
     temp = stdin.readline().strip()
     data.append(split_int(temp))
-print(data)   
+print(dfs(0, 0))
