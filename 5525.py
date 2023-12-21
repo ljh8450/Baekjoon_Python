41d336535da962051f029f34f4872d2477747e2f
ljh8450 <dzigh30@gmail.com>
2023-12-22 03:55:59 +0900

    Update 5525.py
    Web surfing (https://aia1235.tistory.com/30)

--- 5525.py
+++ 5525.py
@@ -4,14 +4,14 @@ n = int(stdin.readline())
 m = int(stdin.readline())
 s = stdin.readline().strip()
 
-def check(n, m, s):
-    cnt = 1
+def check(n, s):
+    cnt = 0
     p = "I"+"OI"*n
     pl = len(p)
-    for i in range(m-2*n):
+    for i in range(len(s)-pl+1):
         if s[i:i+pl] == p:
             cnt += 1
     return cnt
 
-ans = check(n, m, s)
+ans = check(n, s)
 print(ans)
