283b6769e77dcd53d9ed0b43e7b095a94854e33e
ljh8450 <dzigh30@gmail.com>
2023-12-22 01:16:47 +0900

    Update 5525.py
    Misunderstand

--- 5525.py
+++ 5525.py
@@ -1,22 +1,22 @@
 from sys import stdin
 
-s = list(stdin.readline().strip())
+s = stdin.readline().strip()
 n = int(stdin.readline())
-p = ["I"]+["OI"]*n
-
+p = "I"+"OI"*n
 cnt = 0
 
 def delete(s, n, p, cnt):
-    t = len(s)-2*n
 
+    t = len(s)-2*n
     for i in range(0, t):
-        if s[i: i+(2*n+1)] == p:
-            t -= 2*n-1
-            s = s[0:i] + s[i+2*n:t]
-            cnt += 1
-            return cnt
+       if s[i: i+(2*n+1)] == p:
+          t -= 2*n-1
+          s = s[0:i] + s[i+2*n:t]
+          cnt += 1
+          return cnt
+    n = 0
 
 while len(s) > 2*n:
     delete(s, n, p, cnt)
-    
+
 print(cnt)
