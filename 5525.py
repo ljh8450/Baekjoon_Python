764e641b91a350ba6db1c2e2ec747f184e52a32f
ljh8450 <dzigh30@gmail.com>
2023-12-22 03:36:47 +0900

    Update 5525.py
    Half answer

--- 5525.py
+++ 5525.py
@@ -1,22 +1,17 @@
 from sys import stdin
 
-x = int(stdin.readline())
 n = int(stdin.readline())
+m = int(stdin.readline())
 s = stdin.readline().strip()
-p = "I"+"OI"*x
-cnt = 0
 
-def delete(s, n, x, p, cnt):
-    t = n-2*x
-    for i in range(0, t):
-       if s[i: i+(2*x+1)] == p:
-          t -= 2*x-1
-          s = s[0:i] + s[i+2*x:t]
-          cnt += 1
-          return cnt
-    n = 0
+def check(n, m, s):
+    cnt = 1
+    p = "I"+"OI"*n
+    pl = len(p)
+    for i in range(m-2*n):
+        if s[i:i+pl] == p:
+            cnt += 1
+    return cnt
 
-while len(s) > 2*n:
-    delete(s, n, x, p, cnt)
-
-print(cnt)
+ans = check(n, m, s)
+print(ans)
