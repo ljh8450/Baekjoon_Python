8a9edbdc6ef2f0b29584acbdd1b14bdeba0d2aa7
ljh8450 <dzigh30@gmail.com>
2023-12-23 02:07:00 +0900

    Update 5525.py
    Create variable to solve problem(index, cnt, ans)

--- 5525.py
+++ 5525.py
@@ -3,15 +3,19 @@ from sys import stdin
 n = int(stdin.readline())
 m = int(stdin.readline())
 s = stdin.readline().strip()
+index = 0
+cnt = 0
+ans = 0
 
-def check(n, s):
-    cnt = 0
-    p = "I"+"OI"*n
-    pl = len(p)
-    for i in range(len(s)-pl+1):
-        if s[i:i+pl] == p:
-            cnt += 1
-    return cnt
+while index < m - 1:
+    if s[index:index+3] == "IOI":
+        cnt += 1
+        index += 2
+        if cnt == n:
+            ans += 1
+            cnt -= 1
+    else:
+        cnt = 0
+        index += 1
 
-ans = check(n, s)
 print(ans)
