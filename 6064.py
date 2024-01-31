4918b0e7945e6d0d0582f0077ef50d5dafb90745
ljh8450 <dzigh30@gmail.com>
2024-01-31 23:47:33 +0900

    Correct
    ModelCode

--- 6064.py
+++ 6064.py
@@ -2,23 +2,14 @@ from sys import stdin
 
 t = int(stdin.readline())
 
-def checker(m, n, x, y):
-    bet_mn = abs(m-n)
-    bet_xy = abs(x-y)
-    big_num = max(m, n)
-    list_bet = []
-    while True:
-        big_num -= bet_mn
-        if bet_mn in list_bet or big_num in list_bet:
-            return False
-        list_bet.append(big_num)
-        list_bet.append(bet_mn)
-        if bet_mn == bet_xy or big_num == bet_xy:
-            return True
-        print(list_bet)
-        big_num += bet_mn
+def calculater(m, n, x, y):
+    k = x
+    while k < m*n:
+        if (k-x)%m == 0 and (k-y)%n == 0:
+            return k
+        k += m
+    return -1
 
 for _ in range(t):
-    a, b = 0, 0
     m, n, x, y = map(int, stdin.readline().split())
-    print(checker(m, n, x, y))
+    print(calculater(m, n, x, y))
