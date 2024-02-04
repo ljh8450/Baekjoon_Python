cefa86d916a1f5649175a6f991287e13f40c7c77
Miniberry <dzigh30@gmail.com>
2024-02-04 18:59:39 +0900

    Correct!

--- 24.02/13300.py
+++ 24.02/13300.py
@@ -3,7 +3,8 @@ n, k = map(int, input().split())
 info = [[0]*7 for _ in range(3)]
 
 for _ in range(n):
-    info = map(int, input().split())
+    j, y = map(int, input().split())
+    info[j][y] += 1
 
 room = 0
 for i in info:
