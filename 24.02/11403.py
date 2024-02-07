e31dd22bc3b2977fe049d95aab8adc9d7828eef3
Miniberry <dzigh30@gmail.com>
2024-02-07 23:29:43 +0900

    Add DFS

--- 24.02/11403.py
+++ 24.02/11403.py
@@ -24,3 +24,21 @@ for i in range(n):
 
 for i in visited:
     print(*i)
+
+# DFS
+def dfs(x):
+    for i in range(n):
+        if graph[x][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+
+for i in range(n):
+    dfs(i)
+    for j in range(n):
+        if visited[i] == 1:
+            print(1, end=' ')
+        else:
+            print(0, end=' ')
+
+    print()
+    visited = [0 for _ in range(n)]
