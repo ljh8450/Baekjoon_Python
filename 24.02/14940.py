c53820bb36492643f786c8674ad334ca33fd5fd2
Miniberry <dzigh30@gmail.com>
2024-02-08 22:42:25 +0900

    Fix!

--- 24.02/14940.py
+++ 24.02/14940.py
@@ -4,12 +4,13 @@ from collections import deque
 n, m = map(int, stdin.readline().split())
 graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
 visited = [[-1]*m for _ in range(n)]
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 def bfs(i, j):
     queue = deque()
     queue.append((i, j))
     visited[i][j] = 0
+
     while queue:
         x, y = queue.popleft()
         for i in range(4):
@@ -25,10 +26,10 @@ for i in range(n):
     for j in range(m):
         if graph[i][j] == 2 and visited[i][j] == -1:
             bfs(i, j)
-            
+
 for i in range(n):
     for j in range(m):
-        if visited[i][j] == 0:
+        if graph[i][j] == 0:
             print(0, end=' ')
         else:
             print(visited[i][j], end=' ')
