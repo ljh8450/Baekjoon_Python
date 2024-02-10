37fa511ff7eba58eb827588f1c6d5188c6f9ca59
Miniberry <dzigh30@gmail.com>
2024-02-10 13:57:40 +0900

    Correct
    Model Code

--- 24.02/13909.py
+++ 24.02/13909.py
@@ -1,15 +1,1 @@
-from sys import stdin
-
-n = int(stdin.readline())
-data = [0]*(n+1)
-
-def func(x, n):
-    for k in range(x, n+1, x):
-        if data[k-1] == 1:
-            data[k-1] = 0
-        else:
-            data[k-1] = 1
-for i in range(1, n+1):
-    func(i, n)
-
-print(data.count(1))
+print(int(int(input())**0.5))
