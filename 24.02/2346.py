a79b040dcb43578d0fbda8e5b9c109d4ffd9f4f2
Miniberry <dzigh30@gmail.com>
2024-03-03 19:30:08 +0900

    answer!

--- 24.02/2346.py
+++ 24.02/2346.py
@@ -4,29 +4,33 @@ from collections import deque
 input = sys.stdin.readline
 # 변수 설정
 n = int(input())
-data = deque(list(map(int, input().split())))
+queue = deque(list(map(int, input().split())))
+data = deque([x for x in range(1, n+1)])
 index = 0
 ans = list()
 # 차례로 탐색
-for x in range(n):
-    if len(data) <= 1:
-        ans.append(data.popleft())
-        break
-    ans.append(index+1)
-    if index >= len(data):
-        index = index%len(data)
-    temp = index
-    for y in range(data[index]):
-        # 탐색 플로우: +면 popleft&append/ -면 pop&appendleft
-        if data[index] >= 0:
+temp = queue.popleft()
+ans.append(data.popleft())
+for x in range(n-1):
+    check = -1
+    if temp < 0:
+        temp = temp*(-1)
+        check = 0
+    else:
+        check = 1
+    for k in range(temp-1):
+        if check > 0:
+            queue.append(queue.popleft())
             data.append(data.popleft())
-            temp += 1
         else:
+            queue.appendleft(queue.pop())
             data.appendleft(data.pop())
-            temp -= 1
-    if index < 0:
-        temp += n-1
-    data.popleft()
-    index = temp
-for t in ans:
-    print(t, end=' ')
+    if check == 1:
+        ans.append(data.popleft())
+        temp = queue.popleft()
+    else:
+        ans.append(data.pop())
+        temp = queue.pop()
+
+for a in ans:
+    print(a, end=' ')
