f5bca77075da8cc2b411aa6aae91f626437c8d2e
Miniberry <dzigh30@gmail.com>
2024-04-28 12:09:50 +0900

    Answer!
    This code was refered to blog(https://edder773.tistory.com/255)

--- 24.05/2580.py
+++ 24.05/2580.py
@@ -1,49 +1,44 @@
-def is_valid(x, y, num, grid):
-    # 같은 행에 같은 숫자가 있는지 확인
+import sys
+input = sys.stdin.readline
+
+def row(a, n):
     for i in range(9):
-        if grid[x][i] == num:
+        if n == sudoku[a][i]:
             return False
-    # 같은 열에 같은 숫자가 있는지 확인
+    return True
+def column(a, n):
     for i in range(9):
-        if grid[i][y] == num:
+        if n == sudoku[i][a]:
             return False
-    # 3x3 서브그리드에 같은 숫자가 있는지 확인
-    start_row = (x // 3) * 3
-    start_col = (y // 3) * 3
+    return True
+def square(y, x, n):
     for i in range(3):
         for j in range(3):
-            if grid[start_row + i][start_col + j] == num:
+            if n == sudoku[y//3 * 3 + i][x//3 * 3 + j]:
                 return False
     return True
 
-def solve_sudoku(grid):
-    for x in range(9):
-        for y in range(9):
-            if grid[x][y] == 0:  # 빈 칸 찾기
-                for num in range(1, 10):  # 1부터 9까지의 숫자 시도
-                    if is_valid(x, y, num, grid):
-                        grid[x][y] = num
-                        if solve_sudoku(grid):
-                            return True
-                        grid[x][y] = 0
-                return False
-    return True
+def find(n):
+    if n == len(blank):
+        for i in sudoku:
+            print(*i)
+        exit()
+    
+    for i in range(1, 10):
+        y = blank[n][0]
+        x = blank[n][1]
+        if column(x, i) and row(y, i) and square(y, x, i):
+            sudoku[y][x] = i
+            find(n+1)
+            sudoku[y][x] = 0
+                
 
-# 스도쿠 보드 입력
-sudoku = [
-    [0, 3, 5, 4, 6, 9, 2, 7, 8],
-    [7, 8, 2, 1, 0, 5, 6, 0, 9],
-    [0, 6, 0, 2, 7, 8, 1, 3, 5],
-    [3, 2, 1, 0, 4, 6, 8, 9, 7],
-    [8, 0, 4, 9, 1, 3, 5, 0, 6],
-    [5, 9, 6, 8, 2, 0, 4, 1, 3],
-    [9, 1, 7, 6, 5, 2, 0, 8, 0],
-    [6, 0, 3, 7, 0, 1, 9, 5, 2],
-    [2, 5, 8, 3, 9, 4, 7, 6, 0]
-]
+sudoku = [list(map(int, input().split())) for _ in range(9)]
+blank = []
 
-if solve_sudoku(sudoku):
-    for row in sudoku:
-        print(" ".join(map(str, row)))
-else:
-    print("No solution exists")
+for i in range(9):
+    for j in range(9):
+        if sudoku[i][j] == 0:
+            blank.append([i, j])
+
+find(0)
